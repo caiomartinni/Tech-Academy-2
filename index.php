343b2d335248e32c8759f2f170a46776c4d34d44
caiomartinni <caio.martini@grupointegrado.br>
2024-06-02 10:55:25 -0300

    ArcadeHub v4.0

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ArcadeHub: Home</title>
+    
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="shortcut icon" href="imagens/icone.png">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Abel&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/2382e2b1fd.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <header class="header">
+        <img src="imagens/logo.png">
+        <nav class="header-nav">
+            <ul>
+                <li>
+                    <a>
+                        Home
+                    </a>
+                </li>
+                <li>
+                    <a>
+                        Catálogo
+                    </a>
+                </li>
+                <li>
+                    <a>
+                        Sobre
+                    </a>
+                </li>
+                <li>
+                    <a>
+                        Contato
+                    </a>
+                </li>
+            
+    </header>
+    <main>
+        <p>Jogue os melhores jogos disponíveis no ArcadeHub!</p>
+    </main>
+    <footer class="footer">
+        <p>Desenvolvido por Caio Martini.</p>
+        <p>2024. Todos os direitos reservados.</p>
+    </footer>
+</body>
+</html>
