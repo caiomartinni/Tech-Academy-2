cb22114f78d2925c1187d213045249ece0e5a48e
caiomartinni <caio.martini@grupointegrado.br>
2024-06-03 20:41:32 -0300

    ArcadeHub v.7.1

--- paginas/erro.php
+++ paginas/erro.php
@@ -1,5 +1,7 @@
 <div class="erro">
     <h1>Erro!</h1>
     <p>Não foi possível encontrar a página solicitada.</p>
-    <a href="index.php?pagina=home">Voltar ao início</a>
+    <a href="index.php?pagina=home">
+            Voltar ao Início
+    </a>
 </div>
