b423eb70f5d978152e485ac06a0c8e0790067ba6
caiomartinni <caio.martini@grupointegrado.br>
2024-06-09 09:33:00 -0300

    ArcadeHub v9.0
    Mudei a identidade visual do site mas ainda falta muitas coisas pra ajustar

--- index.php
+++ index.php
@@ -1,96 +1,68 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ArcadeHub</title>
-    
+
+    <link rel="shortcut icon" href="imagens/logo/icone.png">
+
+    <!--Fontes: Exo 2, Nunito-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Exo+2:ital,wght@0,100..900;1,100..900&family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap"
+        rel="stylesheet">
+
+    <!--Folhas de Estilo e Bootstrap-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
-    <link rel="shortcut icon" href="imagens/icone.png">
 
-    <!--Fonte: Abel-->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Abel&display=swap" rel="stylesheet">
-
-    <!--Fonte: Ubuntu-->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,100..900;1,100..900&family=Space+Grotesk:wght@300..700&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
-    
-    <!--Bootstrap-->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-
-    <!--Ícones do Fontawesome-->
-    <script src="https://kit.fontawesome.com/2382e2b1fd.js" crossorigin="anonymous"></script>
+    <!--Ícones do Bootstrap-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 </head>
-<body>
-    <header class="header">
-        <img src="imagens/logo.png">
-        <nav class="header-nav">
-            <ul>
-                <li>
-                    <a href="index.php?pagina=home">
-                        Home
-                    </a>
-                </li>
-                <li>
-                    <a href="index.php?pagina=jogos">
-                        Jogos
-                    </a>
-                </li>
-                <li>
-                    <a href="index.php?pagina=sobre">
-                        Sobre
-                    </a>
-                </li>
-                <li>
-                    <a href="index.php?pagina=contato">
-                        Contato
-                    </a>
-                </li>
-            </ul>
-        </nav>
-        <a href="javascript:mostrarMenu()" title="menu">
-            <i class="fa-solid fa-bars"></i>
-        </a>
-    </header>
-        <nav class="mobile-menu">
-        <ul>
-                <li>
-                    <a href="index.php?pagina=home">
-                        Home
-                    </a>
-                </li>
-                <li>
-                    <a href="index.php?pagina=jogos">
-                        Jogos
-                    </a>
-                </li>   
-                <li>
-                    <a href="index.php?pagina=sobre">
-                        Sobre
-                    </a>
-                </li>
-                <li>
-                    <a href="index.php?pagina=contato">
-                        Contato
-                    </a>
-                </li>
-            </ul>
-        </nav>
 
-        <script>
-            function mostrarMenu () {
-                var menu = document.querySelector(".mobile-menu");
-                menu.classList.toggle("show");
-            }
-        </script>
+<body>
+
+    <!--Importar Bootstrap-->
+    <script src="js/bootstrap.bundle.min.js"></script>
+
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <img src="imagens/logo/logo-h-white2.png" alt="ArcadeHub" title="ArcadeHub" width="200px">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="index.php?pagina=home">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php?pagina=jogos">Jogos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Sobre</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contato</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
     <main>
         <?php
         //imprimir o conteúdo do array do GET
         //print_r($_GET);
-
+        
         //recuperar a variável página
         $pagina = $_GET["pagina"] ?? "home";
 
@@ -106,28 +78,10 @@
         ?>
     </main>
 
-    <footer class="footer">
-        <p class="text-center" >Desenvolvido por Caio Martini.</p>
-        <p class="text-center">2024. Todos os direitos reservados.</p>
-        <ul>
-            <li>
-                <a href="https://instagram.com">
-                    <i class="fa-brands fa-instagram"></i>
-                </a>
-            </li>
-            <li>
-                <a href="https://github.com/caiomartinni">
-                    <i class="fa-brands fa-github"></i>
-                </a>
-            </li>
-            <li>
-                <a href="https://linkedin.com">
-                    <i class="fa-brands fa-linkedin"></i>
-                </a>
-            </li>
-        </ul>
+    <footer>
+        <p class="text-center">Desenvolvido por Caio Martini</p>
     </footer>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
 </body>
+
 </html>
--- paginas/jogos.php
+++ paginas/jogos.php
@@ -1,11 +1,80 @@
-<div class="card" style="width: 18rem;">
-  <img src="./imagens/banner/dino.png" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">The Dino Game</h5>
-    <p class="card-text">Controle seu pequeno dinossauro.</p>
-    <a href="#" class="btn btn-primary">
-        <i class="fa-solid fa-plus"></i>
-        Detalhes
-    </a>
-  </div>
+<div class="container">
+
+    <div class="row">
+        <div class="col-xs-12 col-md-6 col-lg-4">
+            <div class="card" style="width: 18rem;">
+                <img src="./imagens/banner/dino.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                        card's
+                        content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-xs-12 col-md-6 col-lg-4">
+            <div class="card" style="width: 18rem;">
+                <img src="./imagens/banner/dino.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                        card's
+                        content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-xs-12 col-md-6 col-lg-4">
+            <div class="card" style="width: 18rem;">
+                <img src="./imagens/banner/dino.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                        card's
+                        content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-xs-12 col-md-6 col-lg-4">
+            <div class="card" style="width: 18rem;">
+                <img src="./imagens/banner/dino.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                        card's
+                        content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-xs-12 col-md-6 col-lg-4">
+            <div class="card" style="width: 18rem;">
+                <img src="./imagens/banner/dino.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                        card's
+                        content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-xs-12 col-md-6 col-lg-4">
+            <div class="card" style="width: 18rem;">
+                <img src="./imagens/banner/dino.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                        card's
+                        content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
